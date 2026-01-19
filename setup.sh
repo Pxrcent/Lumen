819b010ea6e08a86a3d2f7e8d173501033c47308
LowPxrcentage <loolzynwoo@gmail.com>
2026-01-19 10:02:56 -0300

    fixes and improvements

--- setup.sh
+++ setup.sh
@@ -6,12 +6,32 @@ TODAYS="users$(date +%F).csv"
 SETUPFLAG="$MAINDIR/.flag/done.txt"
 GREEN='\033[0;32m'
 NC='\033[0m'
+
 if
 	[[ -f "$SETUPFLAG" ]]; then
-	echo "Setup already ran! Run again with these flags to manage:"
-	echo "--database : to edit the users.csv file" # WIP
-	echo "--reset : to delete and recreate everything"  #WIP
-	exit 0
+	case "$1" in
+	--database)
+	echo "WIP" && exit 0
+	;;
+	--reset)
+	read -p "this will delete EVERYTHING about lumen, do you wish to proceed?(y/n) > " delete
+		case "$delete" in
+	
+			y|Y|yes|Yes|YES)
+			bash /srv/cleaning && exit 5 
+			;;
+			*)
+			echo "exiting!" && exit 1
+			;;
+		esac
+		;;
+		*)
+		echo "Setup already ran! Run again with these flags to manage:"
+		echo "--database: to view/edit the users.csv file" # WIP
+		echo "--reset: to delete and recreate everything"  #WIP
+			exit 0
+			 ;;		
+	esac
 fi
 
 sudo -v
