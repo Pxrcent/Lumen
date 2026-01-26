c7e466ec23a929bd3adee5290c24ef4e471a3bec
LowPxrcentage <loolzynwoo@gmail.com>
2026-01-26 17:22:44 -0300

    fixes and improvements

--- setup.sh
+++ setup.sh
@@ -4,67 +4,102 @@ trap 'echo "❌ Error on line $LINENO: $BASH_COMMAND"' ERR
 MAINDIR=/srv/lumen
 TODAYS="users$(date +%F).csv"
 SETUPFLAG="$MAINDIR/.flag/done.txt"
-GREEN='\033[0;32m'
+GN='\033[0;32m'
+YW='\033[0;33m'
+CN='\033[0;36m'
+PP='\033[0;35m'
 NC='\033[0m'
-
+######################################################################################
 if
 	[[ -f "$SETUPFLAG" ]]; then
 	case "$1" in
 	--database)
-	echo "WIP" && exit 0
+	printf "%b\n" "${PP}WIP${NC}" && exit 0
 	;;
 	--reset)
-	read -p "this will delete EVERYTHING about lumen, do you wish to proceed?(y/n) > " delete
-		case "$delete" in
-	
+	read -p "this will delete EVERYTHING about lumen, do you wish to proceed?(y/N) > " delete
+		case "$delete" in	
 			y|Y|yes|Yes|YES)
 			bash /srv/cleaning && exit 5 
 			;;
 			*)
-			echo "exiting!" && exit 1
+			printf "%b\n" "${PP}exiting!${NC}" && exit 1
 			;;
 		esac
 		;;
 		*)
-		echo "Setup already ran! Run again with these flags to manage:"
-		echo "--database: to view/edit the users.csv file" # WIP
-		echo "--reset: to delete and recreate everything"  #WIP
+		printf "%b\n" "${PP}Setup already ran! Run again with these flags to manage:${NC}"
+		printf "%b\n" "${YW}--database: to view/edit the users.csv file${NC}" # WIP
+		printf "%b\n" "${YW}--reset: to delete and recreate everything${NC}"  #WIP
 			exit 0
 			 ;;		
 	esac
 fi
-
+######################################################################################
 sudo -v
-sudo mkdir -p "$MAINDIR/db" && echo "creating lumen directory..."
+sudo mkdir -p "$MAINDIR/db" && printf "%b\n" "${CN}creating lumen directory...${NC}"
 sudo mkdir "$MAINDIR/backups"
 sudo mkdir "$MAINDIR/.flag"
-echo -e "${GREEN}done${NC}"
+touch "$HOME/done.txt" && echo "#" > "$HOME/done.txt" && sudo mv "$HOME/done.txt" "$SETUPFLAG"
+printf "%b\n" "${GN}done${NC}"
 sleep 1
-sudo groupadd lumen && echo "creating dedicated group..."
-echo -e "${GREEN}done${NC}"
-sudo useradd -g lumen -M -s /usr/bin/nologin lumen && echo "creating dedicated user..."
-echo -e "${GREEN}done${NC}"
+######################################################################################
+sudo groupadd lumuser
+sudo groupadd lumain && printf "%b\n" "${CN}creating dedicated group...${NC}"
 sleep 1
-touch users.csv && sudo mv users.csv "$MAINDIR/db" && echo "creating database..."
-echo -e "${GREEN}done${NC}"
-
-echo "ID,Name,Profession" | sudo tee "$MAINDIR/db/users.csv" && echo "adding basic template to users.csv"
-echo -e "${GREEN}done${NC}"
-
-touch $TODAYS && sudo mv "$TODAYS" "$MAINDIR/backups"
+printf "%b\n" "${GN}done${NC}"
+######################################################################################
+sudo useradd -g lumain -M -s /usr/bin/nologin Lumen && printf "%b\n" "${CN}creating dedicated user...${NC}"
+printf "%b\n" "${GN}done${NC}"
 sleep 1
-sudo chown -R lumen:lumen /srv/lumen && echo "finishing config..."
-touch "done.txt" && sudo mv "done.txt" "$SETUPFLAG"
-echo -e "${GREEN}done${NC}"
-
+######################################################################################
+touch users.csv && sudo mv users.csv "$MAINDIR/db" && printf "%b\n"  "${CN}creating database...${NC}"
+sleep 1
+printf "%b\n" "${GN}done${NC}"
+######################################################################################
+echo "ID,Name,Profession" | sudo tee "$MAINDIR/db/users.csv" && printf "%b\n" "${CN}adding basic template to users.csv ${NC}"
+touch "$TODAYS" && sudo mv "$TODAYS" "$MAINDIR/backups"
+sleep 1
+printf "%b\n" "${GN}done${NC}"
+######################################################################################
+sudo cp "auth.sh" "$MAINDIR"										# fix later
+sudo cp "setup.sh" "$MAINDIR" && printf "%b\n" "${CN}copying files...${NC}" # fix later
+sudo cp "project.json" "$MAINDIR"									# fix later
+sleep 2
+printf "%b\n" "${GN}done${NC}"
+######################################################################################
+# sudo chmod 730 "$MAINDIR/db" && echo "setting permissions..."
+# sudo chmod 720 "$MAINDIR/db/users.csv"
+# printf "%b\n" "${GREEN}done${NC}"
+sleep 1
+######################################################################################
+read -p "do you wish to add $USER to the lumuser group? Y/n > " PERM
+case "$PERM" in
+	n|N|no|No)
+	printf "%b\n" "${YW}skipping...${NC}" ;;
+	*)
+	sudo usermod -aG lumuser $USER && printf "%b\n" "${YW}added successfuly${NC}";;
+esac
+sleep 2
+######################################################################################
+echo " "
+sudo chown -R Lumen:lumain "$MAINDIR" && echo "finishing config..."
+sudo chown Lumen:lumuser "$MAINDIR/auth.sh"
+sudo chown Lumen:lumuser "$MAINDIR/db/users.csv" 
+sleep 2
+printf "%b\n" "${GN}done${NC}"
+######################################################################################
 read -p "setup finished! Type 'database' to manage users.  > " FLAVOUR # WIP
-
 case $FLAVOUR in
 	database|--database|Database|DATABASE)
-	echo "work in progress, thank you for your time!"
+	printf "%b\n" "${PP}work in progress, thank you for your time!${NC}"
+;;
+	sybau|--sybau)
+	printf "%b\n" "${YW}NUKING!!!!!!!!!!!${NC}" && bash "/srv/cleaning"
 ;;
 	*)
-	echo "uh oh, option not found, exiting!"
+	printf "%b\n" "${YW}uh oh, option not found, exiting!${NC}"
 	exit 1
 ;;
 esac
+######################################################################################
